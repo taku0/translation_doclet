1445ecd30c787e2b4ed58acaab7ed7a1527e78da
taku0 <mxxouy6x3m_github@tatapa.org>
2014-02-15 19:15:58 +0900

    Added license information, README.  Removes trailing spaces.

---
+++ README.md
@@ -0,0 +1,19 @@
+# translation_doclet
+
+Enables easy javadoc localization. A free (GPLed) alternative to l10ndoclet ( http://sourceforge.net/projects/l10ndoclet/ ).
+
+Features:
+
+- XML friendly: Generates well-formed XML (including markups in comments) file suitable for XSLT or other XML processing tools.
+- sed friendly: Removes irrevalent spaces from comments suitable for sed or other regexp processing tools.
+- OmegaT friendly: Generates a structured XML file as well as a plain HTML file, suitable for OmegaT or other computer assisted translation tools.
+
+This project provides two docklets and two optional XSLT files. ExtractDoclet extracts comment strings from source into structured XML files. InjectDoclet generates translated javadoc using standard doclet. to\_html.xsl and from\_html.xsl converts between structured XML files and plain HTML files.
+
+Batch files extract.sh and inject.sh are sample batch files for JDK 8 java.util.stream translation.
+
+## License
+
+Copyright 2014 taku0 ( https://github.com/taku0 )
+
+Licensed under GPL 2.0 or later. See LICENSE for details.
--- src/main/scala/org/tatapa/translation_doclet/InjectDoclet.scala
+++ src/main/scala/org/tatapa/translation_doclet/InjectDoclet.scala
@@ -1,3 +1,24 @@
+/*
+ * translation_doclet, helper doclets for translating Javadoc.
+ * Copyright (C) 2014  taku0 ( https://github.com/taku0 )
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Please contact taku0 ( https://github.com/taku0 ) if you need additional
+ * information or have any questions.
+ */
 package org.tatapa.translation_doclet
 
 import com.sun.javadoc._
@@ -118,7 +139,7 @@ object InjectDoclet {
     HtmlDoclet.validOptions(options, reporter) && isFile
   }
 
-  def languageVersion() = 
+  def languageVersion() =
     com.sun.tools.doclets.internal.toolkit.AbstractDoclet.languageVersion()
 
   def start(root: RootDoc): Boolean = {
